a9d84b10c79cfdd8353ccb6350d33f13d8a56148
zhouzhouthezhou <kylezhou2222@gmail.com>
2020-02-22 21:43:37 -0700

    morre idk

--- src/driver.py
+++ src/driver.py
@@ -2,8 +2,12 @@ from pymongo import MongoClient
 
 client = MongoClient('mongodb+srv://haspburn71280:H8IsNoGood@hatebaddb-kbv0e.gcp.mongodb.net/test?retryWrites=true&w=majority')
 db = client.hatebad
-test = db.test
+test = db.hateTestSet
 
-testDoc = {"First Name":"Kyle", "Last Name":"Zhou", "Age":20, "Urmum":"gay"}
+#testDoc = {"First Name":"Kyle", "Last Name":"Zhou", "Age":20, "Urmum":"gay"}
+#test.insert_one(testDoc)
 
-test.insert_one(testDoc)
+x = test.find({})
+
+for i in x:
+	print(type(i['text']))
